b62f334d9d74f7d9e574135cab932b118f5e85f1
David <drichied@protonmail.com>
2023-01-22 19:28:25 -0500

    Add missing files

---
+++ laravel/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <div class="text-xl">
+                            Hello, world!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
